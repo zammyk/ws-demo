bc159502be635239be61946ddcc50d3e613e4d4f
Rahul Hembrom <rhembrom21@gmail.com>
2024-04-28 16:56:09 +0530

    Add user model

---
+++ src/main/java/com/rh/websocketdemo/model/User.java
@@ -0,0 +1,17 @@
+package com.rh.websocketdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private UUID id;
+    private String username;
+}
