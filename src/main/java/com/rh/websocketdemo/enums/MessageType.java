32138d32d3bd7407b31adbf16613734ccb30a0f7
Rahul Hembrom <rhembrom21@gmail.com>
2024-11-21 23:18:45 +0530

    Update Message Dto

---
+++ src/main/java/com/rh/websocketdemo/enums/MessageType.java
@@ -0,0 +1,7 @@
+package com.rh.websocketdemo.enums;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
--- src/main/java/com/rh/websocketdemo/model/Message.java
+++ src/main/java/com/rh/websocketdemo/model/Message.java
@@ -1,15 +1,23 @@
 package com.rh.websocketdemo.model;
 
+import com.rh.websocketdemo.enums.MessageType;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
-@Data
+// TODO: find a way to persist these messages
 @Builder
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Message {
-    private UUID senderId;
-    private UUID receiverId;
+    private String id;
+    private String sender;
+    private String receiver;
+    // TODO: make a separate message class
+    private MessageType type;
     private String message;
 }
